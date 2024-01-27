477805e1cec577cd98cf105ad9db52446242224a
BygidHasan <bygidhasan6@gmail.com>
2024-01-27 22:01:51 +0600

    new start for server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express()
+dotenv.config()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send("Hello world");
+})
+
+app.listen(port, () => console.log(`server is running at http://localhost:${port}`))
